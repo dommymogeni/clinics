a7417d0e6828096577952df45053871d0bee2ca5
dommymogeni <dominicmogeni1998@gmail.com>
2024-08-15 09:18:58 +0300

    updated: databse config

--- auth.ts
+++ auth.ts
@@ -1,8 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
 import NextAuth from "next-auth"
+import prisma from "@/lib/db"
 
-const prisma = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
