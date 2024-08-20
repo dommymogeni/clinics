e12eff4071ffd0af056ae9491019045437c145b2
dommymogeni <dominicmogeni1998@gmail.com>
2024-08-20 11:45:06 +0300

    updated the primary landing page

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import NextAuth from "next-auth";
 import db from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-export const { handlers:{GET, POST}, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
